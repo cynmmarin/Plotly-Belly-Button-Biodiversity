6e50f4ee675ff4a7eedfccb46c67dfcc8ba06d62
Cynthia Marin <cynthiammarin@gmail.com>
2021-07-23 04:38:42 -0700

    Deliverable 1 and 2

--- Module_Coursework /CourseWork/README.md
+++
@@ -1,2 +0,0 @@
-# Plotly-Belly-Button-Biodiversity
-Module 12: Plotly &amp; Belly Button Biodiversity
--- js/charts.js
+++ js/charts.js
@@ -4,7 +4,7 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("Resources/samples.json").then((data) => {
-    console.log(data);
+    //console.log(data);
     var sampleNames = data.names;
     sampleNames.forEach((sample) => {
       selector
@@ -12,7 +12,12 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-})}
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
 
 // Initialize the dashboard
 init();
@@ -26,7 +31,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("Resources/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -36,20 +41,20 @@ function buildMetadata(sample) {
 
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
-    PANEL.append("h6").text(result.location);
-  });
-}
+ 
+
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-    //Object.entries(result).forEach(([key, value]) => {
-      //PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    //});
-
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  });
+}
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("Resources/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.samples; 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
@@ -66,13 +71,12 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_ids.slice(0,10).map(_id => 'Otu ${id}').reverse();
+    var yticks = otu_ids.slice(0,10).map( otu_ID => 'Otu ${otu_ID}').reverse();
 
     // 8. Create the trace for the bar chart. 
     var barTrace = {
       x: sample_values.slice(0,10).reverse(),
       y: yticks,
-      text: otu_labels,
       type: 'bar',
       orientation:'h'
     };
@@ -82,7 +86,7 @@ function buildCharts(sample) {
     var barLayout = {
     title: "Top 10 Bacteria Species",
     xaxis: {title:"Sample Values"},
-    yaxis: {title:"ID's"}
+    yaxis: {title:"Otu ID"}
      
     };
     // 10. Use Plotly to plot the data with the layout. 
